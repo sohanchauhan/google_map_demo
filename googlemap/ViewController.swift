7f8e9e53c2c2c8f47ad286f0fc22a9175e7304a8
sohan chauhan <sohanchauhan@sohans-MacBook-Air.local>
2018-03-10 00:15:51 +0530

    first time adding ready code to repository

---
+++ googlemap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  googlemap
+//
+//  Created by sohan chauhan on 09/03/18.
+//  Copyright © 2018 sohan chauhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
